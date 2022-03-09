ca94f63262a5d1cc846312de7a1c13bae8a2a7c1
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-09 18:07:06 +0500

    Finish header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import  MEN from './../../assets/men.png'
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm </h5> 
+        <h1>Anvarov Mukhammad</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="men">
+          <img src={MEN} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
+import { BsLinkedin, BsGithub, BsTelegram } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+        <BsLinkedin />  
+      </a>
+      <a href="https://github.com" target="_blank" rel="noreferrer">
+        < BsGithub />  
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <BsTelegram />
+      </a>
+  </div>;
+};
+
+export default HeaderSocials;
