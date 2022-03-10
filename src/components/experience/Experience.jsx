c332e23212b1a66142808d1751b0aaa7d8c260e2
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-10 07:03:21 +0500

    Added tooltip to nav

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -43,7 +43,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>Storybook/Formic</h4>
+                <h4>Storybook/Formik</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,41 +4,53 @@ import "./nav.css";
 import { AiOutlineHome, AiOutlineUser } from  'react-icons/ai';
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 const Nav = () => {
   const [activeNav, setActiveNav] = React.useState('#');
   return (
     <nav>
-      <a href="#"
-      onClick={() => setActiveNav('#')}
-      className={activeNav === '#' ? 'active' : ''}
-      >
-        <AiOutlineHome />
-      </a>
-      <a href="#about"
-      onClick={() => setActiveNav('#about')}
-      className={activeNav === '#about' ? 'active' : ''}
-      >
-        <AiOutlineUser />
-      </a>
-      <a href="#experience"
-      onClick={() => setActiveNav('#experience')}
-      className={activeNav === '#experience' ? 'active' : ''}
-      >
-        <BiBook />
-      </a>
-      <a href="#services"
-      onClick={() => setActiveNav('#services')}
-      className={activeNav === '#services' ? 'active' : ''}
-      >
-        <RiServiceLine />
-      </a>
-      <a href="#contact"
-      onClick={() => setActiveNav('#contact')}
-      className={activeNav === '#contact' ? 'active' : ''}
-      >
-        <BiMessageSquareDetail />
-      </a>
+        <Tooltip  placement="top-start" title="Home">
+          <a href="#"
+          onClick={() => setActiveNav('#')}
+          className={activeNav === '#' ? 'active' : ''}
+          >
+            <AiOutlineHome />
+          </a>
+        </Tooltip>
+        <Tooltip  placement="top" title="About">
+          <a href="#about"
+          onClick={() => setActiveNav('#about')}
+          className={activeNav === '#about' ? 'active' : ''}
+          >
+            <AiOutlineUser />
+          </a>
+        </Tooltip>
+        <Tooltip  placement="top" title="Experience">
+          <a href="#experience"
+          onClick={() => setActiveNav('#experience')}
+          className={activeNav === '#experience' ? 'active' : ''}
+          >
+            <BiBook />
+          </a>
+        </Tooltip>
+        <Tooltip  placement="top" title="Services">
+          <a href="#services"
+          onClick={() => setActiveNav('#services')}
+          className={activeNav === '#services' ? 'active' : ''}
+          >
+            <RiServiceLine />
+          </a>
+        </Tooltip>
+        <Tooltip  placement="top-end" title="Contact">
+          <a href="#contact"
+          onClick={() => setActiveNav('#contact')}
+          className={activeNav === '#contact' ? 'active' : ''}
+          >
+            <BiMessageSquareDetail />
+          </a>
+        </Tooltip>
     </nav>
   )
 };
