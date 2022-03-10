6bed9ef893162d274fbffa38ede93fdce599f7ad
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-10 13:53:58 +0500

    Finished contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import './contact.css'
+import { MdOutlineMail } from 'react-icons/md';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { BsInstagram } from 'react-icons/bs';
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return <section id="contact">
+    <h5>Get In Touch</h5>
+    <h2>Contact Me</h2>
+
+    <div className="container contact__container">
+      <div className="contact__options">
+        <article className="contact__option">
+          <MdOutlineMail className="contact__option-icon" />
+          <h4>Email</h4>
+          <h5>Anvarovmuxammadxuja2001@gmail.com</h5>
+          <a href="mailto:anvarovmuxammadxuja2001@gmail.com" target="_blank">Send a message</a>  
+        </article>
+        <article className="contact__option">
+          <FaTelegramPlane className="contact__option-icon" />
+          <h4>Telegram</h4>
+          <h5>@anvarov2027</h5>
+          <a href="https://t.me:/akzmsh" target="_blank">Send a message</a>  
+        </article>
+        <article className="contact__option">
+          <BsInstagram className="contact__option-icon" />
+          <h4>Instagram</h4>
+          <h5>akzmsh</h5>
+          <a href="https://instagram.com/akzmsh" target="_blank">Send a message</a>  
+        </article>
+      </div>
+
+      <form>
+      <input type="text" name="name" placeholder="Your Full Name" required />
+        <input type="email" name="email" placeholder="Your Email" required />
+        <textarea name="message" rows="7" placeholder="Your Message"></textarea>
+        <button type="submit" className="btn btn-primary">Send Message</button>
+
+      </form>  
+    </div>
+  </section>;
 };
 
 export default Contact;
