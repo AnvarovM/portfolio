c0f2cf509553ed09f0b9a9f3862908bd29a53fa3
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-10 13:54:25 +0500

    Finished portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import './portfolio.css'
+import JizzaxCity from "./../../assets/jizzaxcity.png";
+
+const portfolioData = [
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+  {
+    id: 1,
+    image: JizzaxCity,
+    title: 'Jizzax City`s Website',
+    github: 'https://github.com/anvarovM',
+    demo: 'https://jizzaxcity-uz.vercel.app',
+  },
+]
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return <section id="portfolio">
+    <h5>My Resent Work</h5>
+    <h2>Portfolio</h2>  
+
+    <div className="container portfolio__container">
+      {portfolioData.map(({ id, image, title, github, demo }) => {
+        return (
+          <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />  
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className="btn">Git Hub</a>
+              <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  </section>;
 };
 
 export default Portfolio;
