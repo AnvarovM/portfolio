c5a2fff49256ce2fd42c906798887998589ca5cf
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-10 14:17:08 +0500

    Finished footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import './footer.css'
+import { BsLinkedin, BsGithub, BsTelegram } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>
+    <a href="#" className="footer__logo">AKZMSH</a>
+    
+    <ul className="permalinks">
+      <li><a href="">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  
+    <div className="footer__socials">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+        <BsLinkedin />  
+      </a>
+      <a href="https://github.com" target="_blank" rel="noreferrer">
+        < BsGithub />  
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <BsTelegram />
+      </a> 
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy; akzmsh. All rights reserved</small>  
+    </div>
+  </footer>;
 };
 
 export default Footer;
