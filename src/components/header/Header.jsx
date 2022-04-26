6c15cdf3050a5d2028e0b5cf9d52da470b8b5998
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-04-26 13:03:11 +0500

    Renamed name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I &apos; m </h5>
-        <h1>Anvarov Mukhammad</h1>
+        <h1>Anvarov Muhammad</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials />
